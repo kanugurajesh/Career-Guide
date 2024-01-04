1acf8aacb815f91a02911fff558111bab2dacc46
kanugurajesh <245122733172@mvsrec.edu.in>
2024-01-04 18:45:33 +0530

    Adding the updated one

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -7,12 +7,12 @@ import { Typewriter } from 'react-simple-typewriter'
 
 export default function Home() {
     return (
-        <main className="p-10 flex justify-around items-center h-screen">
+        <main className="px-10 flex justify-around items-center h-screen">
             <div className="flex flex-col gap-6">
                 <h1 className="font-black text-6xl">
                     <span style={{ color: 'black', fontWeight: 'bold'}}>
                         <Typewriter
-                            words={['What is Career Aid', 'AI Based guidance', '']}
+                            words={['What is Career Aid', 'AI Based guidance']}
                             loop={1000}
                             cursor
                             cursorStyle='_'
@@ -22,7 +22,7 @@ export default function Home() {
                         />
                     </span>
                 </h1>
-                <p className="text-xl">Swap ai is a generative ai tool to swap your images with your fantasy avatar</p>
+                <p className="text-xl">Career Aid is a interactive AI based model to help students choose careers and know their capabilities</p>
                 <Link href="/Home" className={`font-bold text-xl max-w-max py-3 px-10 rounded-sm transition-all ease-in-out duration-400 ${styles.button} relative`}>
                     <p className={styles.p}>Get Started</p>
                 </Link>
