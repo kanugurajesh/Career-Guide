c51f2da4fcf94af70d43bc33ee5c16756daa3720
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-21 12:55:54 +0530

    Adding contact changes

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -1,14 +1,33 @@
 import * as React from 'react';
 
 interface EmailTemplateProps {
-    imageUrl: string;
+  imageURl: string,
+  email: string
+}
+
+interface ContactEmailTemplateProps {
+  email: string,
+  subject: string,
+  message: string
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  imageUrl,
+  imageURl, email
 }) => (
   <div>
-    <h1>Here is your image!</h1>
-    <img src={imageUrl} alt="Generated Image" />
+    <h1>Here is your image</h1>
+    <h2>Email is {email}</h2>
+    <img src={imageURl} alt="image" />
+  </div>
+);
+
+export const ContactEmailTemplate: React.FC<Readonly<ContactEmailTemplateProps>> = ({
+  email, subject, message
+}) => (
+  <div>
+    <h1>You got a message from Mentify</h1>
+    <h2>Email is {email}</h2>
+    <h2>Subject is {subject}</h2>
+    <h2>Message is {message}</h2>
   </div>
 );
