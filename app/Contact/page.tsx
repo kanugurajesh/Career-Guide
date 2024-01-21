1f064b51c14aeac5bd84685aba7188c94486534b
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-21 15:27:23 +0530

    Updating the contact fields

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -35,6 +35,7 @@ export default function Contact() {
   };
 
   const handleSubmit = async () => {
+    
     if (validateForm()) {
       try {
         const response = await fetch("/api/contact", {
